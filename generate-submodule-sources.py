9820fecbfa9e18523290c5e136cc298fd930fc24
Adam <classygopher@gmail.com>
2025-01-24 23:22:59 +0000

    get rid of bad submodules

--- generate-submodule-sources.py
+++ generate-submodule-sources.py
@@ -4,7 +4,7 @@ import os
 import sys
 import subprocess
 
-def parse_gitmodules(root, relroot = None, out = None):
+def parse_gitmodules(root, root_commit, relroot = None, out = None):
     if not out:
         out = []
     
@@ -25,14 +25,19 @@ def parse_gitmodules(root, relroot = None, out = None):
             dest = line[7:]
             nextroot = os.path.join(root, dest)
             result = subprocess.run(["git", "-C", nextroot, "rev-parse", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+            # nonzero exit code probably means the directory doesn't exist at all
             if result.returncode == 0:
-                if relroot:
-                    module["dest"] = os.path.join(relroot, dest)
-                else:
-                    module["dest"] = dest
-                module["commit"] = result.stdout.decode("utf-8").strip()
-                out.append(module)
-                parse_gitmodules(nextroot, dest, out)
+                commit = result.stdout.decode("utf-8").strip()
+                # if the directory exists, but rev-parse returned the same hash as it did on the level above,
+                # that means the directory isn't a git repo in itself, so we should ignore it
+                if commit != root_commit:
+                    if relroot:
+                        module["dest"] = os.path.join(relroot, dest)
+                    else:
+                        module["dest"] = dest
+                    module["commit"] = commit
+                    out.append(module)
+                    parse_gitmodules(nextroot, commit, dest, out)
         elif line.startswith("url = "):
             module["url"] = line[6:]
 
@@ -48,7 +53,8 @@ if __name__ == "__main__" and argc > 0:
         exit(1)
 
     root = sys.argv[1]
-    modules = parse_gitmodules(root)
+    result = subprocess.run(["git", "-C", root, "rev-parse", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    modules = parse_gitmodules(root, result.stdout.decode("utf-8").strip())
     
     f = open("chromium-submodules.yaml", "w")
     for module in modules:
